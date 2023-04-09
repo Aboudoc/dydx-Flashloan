7d173527fe8baaa44e128d7ed2d101a82d0d9b4f
Aboudoc <aboutika.reda@gmail.com>
2023-04-09 12:25:50 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# DyDx Solo Margin - flashloan
+
+## Setup the project
+
+```shell
+npm init -y
+npm install --save-dev hardhat
+npx hardhat
+```
+
+```shell
+npm i @openzeppelin/contracts@^4.3.2
+```
+
+## Learn more about DyDx
+
+Market ID's
+0 WETH
+1 SAI
+2 USDC
+3 DAI
+
+On DyDx the fees that you have to pay for flashloan is **2 wei**
+
+The way you request a flashloan on DyDx is by creating 3 actions:
+
+- Withdraw
+- Call callFunction()
+- Deposit back
+
+Once we created these 3 actions, we submit it over to the `solo` contract by calling `operate()` and passing in the 3 actions
---
+++ contracts/DyDxSoloMargin.sol
@@ -0,0 +1,62 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8;
+pragma experimental ABIEncoderV2;
+
+import "./interfaces/DydxFlashloanBase.sol";
+import "./interfaces/ICallee.sol";
+
+contract TestDydxFlashloan is ICallee, DydxFlashloanBase {
+    address private constant SOLO = 0x1E0447b19BB6EcFdAe1e4AE1694b0C3659614e4e;
+    address public flashUser;
+
+    event Log(string message, uint val);
+
+    struct MyCustomData {
+        address token;
+        uint repayAmount;
+    }
+
+    function initiateFlashloan(address _token, uint _amount) external {
+        ISoloMargin solo = ISoloMargin(SOLO);
+
+        uint marketId = _getMarketIdFromTokenAddress(SOLO, _token);
+
+        uint repayAmount = _getRepaymentAmountInternal(_amount);
+        IERC20(_token).approve(SOLO, repayAmount);
+
+        Actions.ActionArgs[] memory operations = new Actions.ActionArgs[](3);
+        // 3 operations
+        operations[0] = _getWithdrawAction(marketId, repayAmount);
+        bytes memory data = abi.encode(
+            MyCustomData({token: _token, repayAmount: repayAmount})
+        );
+        operations[1] = _getCallAction(data);
+        operations[2] = _getDepositAction(marketId, repayAmount);
+
+        Account.Info[] memory accountInfo = new Account.Info[](1);
+        accountInfo[0] = _getAccountInfo();
+
+        solo.operate(accountInfo, operations);
+    }
+
+    function callFunction(
+        address sender,
+        Account.Info memory account,
+        bytes memory data
+    ) public override {
+        require(msg.sender == SOLO, "!solo");
+        require(sender == address(this), "!this contract");
+
+        MyCustomData memory mcd = abi.decode(data, (MyCustomData));
+        uint repayAmount = mcd.repayAmount;
+
+        uint bal = IERC20(mcd.token).balanceOf(address(this));
+        require(bal >= repayAmount, "bal < repayAmount");
+
+        // Custom code - Arbitrage
+        flashUser = sender;
+        emit Log("bal", bal);
+        emit Log("repay", repayAmount);
+        emit Log("profit", bal - repayAmount);
+    }
+}
